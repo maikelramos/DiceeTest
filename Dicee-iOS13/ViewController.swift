9410e15519e457a4bdae5ab47afbbe598f7a1912
Maikel Ramos <mkramost@gmail.com>
2021-11-10 22:22:46 -0500

    die added

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //#imageLiteral(
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        //diceImageView1 .alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        //diceImageView1 .alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
